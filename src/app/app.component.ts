3e0675a828253a16645e38d1b20e19345f0d9954
Mathias Larsen <mathias.larsen@student.uib.no>
2023-10-24 15:33:52 +0200

    edited component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,27 +18,20 @@ export class AppComponent {
     }
 
     refreshTodos() {
-        // Replace 'YOUR_API_URL_HERE' with the actual URL of your Spring Boot API
         this.http.get<any[]>('http://localhost:8080/todos').subscribe((response) => {
             this.todos = response;
         });
     }
 
     addTodo() {
-        // Replace 'YOUR_API_URL_HERE' with the actual URL of your Spring Boot API
         this.http.post('http://localhost:8080/todos', this.newTodo).subscribe(() => {
-            // Refresh the todos list after adding a new todo
             this.refreshTodos();
-
-            // Optionally, you can reset the form
             this.newTodo = {};
         });
     }
 
     deleteTodo(id: number) {
-        // Replace 'YOUR_API_URL_HERE' with the actual URL of your Spring Boot API
         this.http.delete(`http://localhost:8080/todos/${id}`).subscribe(() => {
-            // Refresh the todos list after deleting a todo
             this.refreshTodos();
         });
     }
